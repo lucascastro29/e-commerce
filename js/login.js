94d304df4777442fe6541642f8d1383fb6225659
lucascastro29 <lucascastro2929@gmail.com>
2021-08-18 13:05:47 -0300

    asd

--- js/login.js
+++ js/login.js
@@ -2,5 +2,13 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+document.getElementById("submitb").addEventListener("click",function(e){
+    
+    let inputEmail=document.getElementById("Email");
+    let inputPassword=Document.getElementById("password");
 
+    if(inputEmail.value===true && inputPassword.value===true){ 
+    window.location.href= "index.html";
+    }
+})
 });
