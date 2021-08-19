cf3084867f7ed0188ef416af497b241dbb6392d5
lucascastro29 <lucascastro2929@gmail.com>
2021-08-19 19:05:47 -0300

    1

--- js/login.js
+++ js/login.js
@@ -14,23 +14,5 @@ document.addEventListener("DOMContentLoaded", function(e) {
   
    
 })
-var getJSONDATA = function(url){
-  
-  var result={}
-  return fetch(url).then(response=>{
-    if(response.ok){
-      return response.json()
-    }else{
-      throw Error(response.statusText)
-    }
-  }).then(function(response){
-    result.status="ok"
-    result.data=response
-    return result
-  }).catch(function(error){
-    result.status="error"
-    result.data=error
-    return result
-  })
-}
+
 
--- js/products.js
+++ js/products.js
@@ -1,6 +1,38 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
-});
+const urlproducts="https://lucascastro29.github.io/Autos/";
+
+var getJSONDATA = function(url){
+    var result={}
+    return fetch(url).then(response=>{
+      if(response.ok){
+        return response.json()
+      }else{
+        throw Error(response.statusText)
+      }
+    }).then(function(response){
+      result.status="ok"
+      result.data=response
+      return result
+    }).catch(function(error){
+      result.status="error"
+      result.data=error
+      return result
+    })
+  }
+
+
+function showproducts(array){
+
+}
+
+
+  document.addEventListener("DOMContentLoaded", function (e) {
+     getJSONDATA(urlproducts).then(function(result){
+         if(result.status==="ok"){
+             products=result.data;
+             showproducts(products)
+         }
+     })
+     
+      
+  })
