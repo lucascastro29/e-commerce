f04a633c37e04bf175dd959664c309a3d10eb767
lucascastro29 <lucascastro2929@gmail.com>
2021-08-19 21:25:13 -0300

    ahora si xd

--- js/products.js
+++ js/products.js
@@ -1,6 +1,4 @@
 
-const urlproducts="https://lucascastro29.github.io/Autos/";
-
 var getJSONDATA = function(url){
     var result={}
     return fetch(url).then(response=>{
@@ -20,14 +18,26 @@ var getJSONDATA = function(url){
     })
   }
 
-
 function showproducts(array){
+    let content=''
 
+    array.forEach(element => {
+        content +='<div>'
+        content +='<img src="'+element.imgSrc+'" alt=""></img><br>'
+        content +='Nombre: '+ element.name+'<br>';
+        content +='Precio: '+ element.currency+element.cost+'<br>';
+        content +='vendidos: '+element.soldCount+'<br>';
+        content +="Descripción: "+element.description+'<br>';
+        content +='</div>';
+        
+        
+    });
+    document.getElementById("mainproducts").innerHTML=content;
 }
 
 
   document.addEventListener("DOMContentLoaded", function (e) {
-     getJSONDATA(urlproducts).then(function(result){
+     getJSONDATA(PRODUCTS_URL).then(function(result){
          if(result.status==="ok"){
              products=result.data;
              showproducts(products)
