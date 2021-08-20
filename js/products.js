247df20371b38e41a622254fd18a86330be70827
lucascastro29 <lucascastro2929@gmail.com>
2021-08-20 10:24:35 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,22 +1,5 @@
 
-var getJSONDATA = function(url){
-    var result={}
-    return fetch(url).then(response=>{
-      if(response.ok){
-        return response.json()
-      }else{
-        throw Error(response.statusText)
-      }
-    }).then(function(response){
-      result.status="ok"
-      result.data=response
-      return result
-    }).catch(function(error){
-      result.status="error"
-      result.data=error
-      return result
-    })
-  }
+
 
 function showproducts(array){
     let content=''
