0dc5b09065213cff14187f5629f0e9877055a0bb
lucascastro29 <lucascastro2929@gmail.com>
2021-10-13 19:45:24 -0300

    a

--- js/cart.js
+++ js/cart.js
@@ -1,25 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-CART
+
 function showlist(array){
+    let contents=``
     for (let i = 0; i < array.length; i++) {
         const element = array.articles[i];
         
-        content=`<div class="row">
+        contents+=`<div class="row">
         <div class="col-3" style="height:"100px"; "><img src="${element.src}"></div>
         <div class="col-3"><p>${element.name}</p><br><p><strong>${element.currency} ${element.unitCost}</strong></p></div>
         <div class="col-2"><input type="number" value="${element.count}" style="width: 50px; text-align: end;"></div>
         <div class="col-2"><p>${element.cost*element.unitCost}</p></div>
         </div>
         `
-        document.getElementById("items").innerHTML+=content
+        
     }
-
+    document.getElementById("items").innerHTML+=contents
 }
 
 
-document.addEventListener("DOMContentLoaded", function (e){
+document.addEventListener("DOMContentLoaded", function (){
     getJSONData(CART).then(function(result){
       showlist(result.data)        
     })
