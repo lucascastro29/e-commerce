48ebe69d9b0dc8762b02630f0ce93ba4c890e326
lucascastro29 <lucascastro2929@gmail.com>
2021-10-13 18:53:12 -0300

    1

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+function showproducts(array){
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+        
+    }
+
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
