0d4565a4cdf95ea33cbe062dd168cb5982b588e7
lucascastro29 <lucascastro2929@gmail.com>
2021-10-17 14:55:50 -0300

    1

--- js/cart.js
+++ js/cart.js
@@ -5,27 +5,50 @@ function total(){
   let cont=0
 for (let i = 0; i < 2; i++) {
   ;
-  cont+=parseInt(document.getElementById('suma'+i+'').value);
+  cont+=parseInt(document.getElementById('suma'+i+'').innerHTML);
 
       
 }
-document.getElementById('totalv').value=cont;
+document.getElementById('totalv').innerHTML=cont;
 }
 function np() {
+  let cont=0
   for (let i = 0; i < 2; i++) {
-    ;
+    
     cont+=parseInt(document.getElementById('value'+i+'').value);
+
   
         
   }
-  document.getElementById('np').innerHTML=cont;
+  document.getElementById("nps").innerHTML=cont;
 
 }
 function cost(i,unit){
   let cont=document.getElementById('value'+i+'').value;
-  document.getElementById('suma'+i+'').value=cont*unit;
+  document.getElementById('suma'+i+'').innerHTML=cont*unit;
   total()
   np()
+}
+
+function envio(a){
+  let cont=""
+  if (a===1){ cont="Gratis: 2-4 Dias"
+    document.getElementById("envios").innerHTML=cont
+  }else{
+    cont="Premium: 1 Dia"
+    document.getElementById("envios").innerHTML=cont
+  }
+}
+function envio1(a){
+  let cont=""
+  if (a===1){ cont="Debito: Visa"
+    document.getElementById("envios1").innerHTML=cont
+  }else{
+    cont="Debito: MasterCard"
+    document.getElementById("envios1").innerHTML=cont
+  }if (a===0){ cont="Efectivo "
+  document.getElementById("envios1").innerHTML=cont
+}
 }
 
 
@@ -39,11 +62,12 @@ function showlist(array){
           sub=40*element.unitCost
         }else{sub=element.unitCost}
 
-        content+=`<div class="row border rounded" style="height:160px; padding:5px; margin:15px;">
-        <div class="col-4 imgfather" style="margin:15px; " ><img src="${element.src}" style=";height:100%;;width:100%;"></div>
-        <div class="col-4" stile="height:10px"><p>${element.name}</p><br><p><strong>${element.currency} ${element.unitCost}</strong></p></div>
-        <div class="col-1"><input id="value${i}" type="number" value="${element.count}" onchange="cost(${i},${sub})" style="width: 40px; text-align: end;"></div>
-        <div class="col-2" style="margin-left:15px;"><input id="suma${i}"  type="text" value="${element.count*sub}"  style="width: 80px; text-align: end; border:none;" /><input type="hidden" value="0"/></div>
+        content+=`<div class="row border rounded" style="height:160px;; margin:10px;padding:5px;">
+        <div class="col-4 imgfather"><img src="${element.src}" style=";height:100%;;width:100%;"></div>
+        <div class="col-4" style="height:10px"><p>${element.name}</p><br><p><strong>${element.currency} ${element.unitCost}</strong></p></div>
+        <div class="col-2"><input id="value${i}" type="number" value="${element.count}" onchange="cost(${i},${sub})" style="width: 40px; text-align: end;margin-left:15px;"></div>
+        <div class="col-0" style="width:3px;margin-left:15px;" >$</div>
+        <p><div class="col-1"id="suma${i}">${element.count*sub}</div></p></div>
         </div>
         `
         
